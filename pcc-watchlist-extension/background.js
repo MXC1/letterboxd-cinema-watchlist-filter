d24bb1f161eff747e73a1feb458c0a4a102a59ab
MXC1 <mfhcor@gmail.com>
2025-10-19 11:51:55 +0100

    Revert to using popup instead of options page

--- pcc-watchlist-extension/background.js
+++ pcc-watchlist-extension/background.js
@@ -1,11 +1,1 @@
 // Empty background script for manifest v3
-chrome.action.onClicked.addListener(() => {
-  console.log('Action button clicked');
-  chrome.storage.sync.get(['showWatchlist'], (result) => {
-    console.log('Current showWatchlist value:', result.showWatchlist);
-    const newState = !result.showWatchlist;
-    chrome.storage.sync.set({ showWatchlist: newState }, () => {
-      console.log('Updated showWatchlist to:', newState);
-    });
-  });
-});
